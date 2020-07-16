7f070aa86a14301205162d89c62768ba621c7d65
luningcowboy <luningcowboy@gmail.com>
2020-07-16 11:56:38 +0800

    add new code

--- 2-variables/main.dart
+++ 2-variables/main.dart
@@ -4,4 +4,8 @@ main() {
   dynamic name2 = 'Bob';
   String name3 = 'Bob';
   print('output==>name${name},name2${name2},name3${name3}');
+  int line;
+  print(line);
+  // assert 在非true的情况下抛出异常
+  assert(line == null);
 }
